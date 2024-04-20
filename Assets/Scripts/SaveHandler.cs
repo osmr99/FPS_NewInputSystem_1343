4cee411d67a8beef409a5812dcb08974f890445b
osmr99 <omarsantiagomorales@gmail.com>
2024-04-19 21:22:02 -0500

    Fixed jeff hitbox and Save handler with keybinds!

---
+++ Assets/Scripts/SaveHandler.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.IO;
+using JetBrains.Annotations;
+
+public class SaveHandler : MonoBehaviour
+{
+
+    string path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = Application.persistentDataPath + "/save.json";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.zKey.wasPressedThisFrame)
+        {
+            SaveData sd = new SaveData();
+            sd.playerPosition = FindObjectOfType<FPSController>().transform.position;
+
+            string jsonText = JsonUtility.ToJson(sd);
+            File.WriteAllText(path, jsonText);
+        }
+
+        if (Keyboard.current.xKey.wasPressedThisFrame)
+        {
+            string saveText = File.ReadAllText(path);
+            SaveData myData = JsonUtility.FromJson<SaveData>(saveText);
+            FindObjectOfType<CharacterController>().enabled = false;
+            FindObjectOfType<FPSController>().transform.position = myData.playerPosition;
+            FindObjectOfType<CharacterController>().enabled = true;
+        }
+    }
+}
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+}
